fcaeaa0975b311a64123d3c25941765bbd785a83
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-08-15 16:12:01 +0700

    Fixed msvc builds

--- src/include/random.hpp
+++ src/include/random.hpp
@@ -8,7 +8,7 @@ int getRandomCard(int start = 0, int end = 51) {
   static std::seed_seq ss{rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
   static std::mt19937 mt{ss};
 
-  std::uniform_int_distribution card{start, end};
+  std::uniform_int_distribution<int> card{start, end};
   return card(mt);
 };
 
